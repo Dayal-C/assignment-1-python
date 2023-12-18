bfe057552a325f172af43ca19fe5c728a4bc3955
Dayal-C <145312735+Dayal-C@users.noreply.github.com>
2023-12-18 21:58:28 +0530

    Add files via upload

---
+++ LargestNumber.py
@@ -0,0 +1,10 @@
+x=int(input("Enter the first number"))
+y=int(input("enter the second number"))
+z=int(input("enter the third number"))
+
+if(x>=y)and(x>=z):
+    print(x," is the greater number ")
+if(y>=x)and(y>=z):
+    print(y," is the greater number ")
+if(z>=y)and(z>=x):
+    print(z," is the greater number ")
